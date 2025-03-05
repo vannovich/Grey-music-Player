b71980430b884eeb267f843e5f64c7c1b456c6ce
Awontu Vannovich <awontuvannovich@gmail.com>
2025-03-05 14:22:13 +0100

    Done with songCard

--- src/components/songCard/AlbumInfo.jsx
+++ src/components/songCard/AlbumInfo.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import "./AlbumInfo.css";
-export default function AlbumInfo() {
+
+export default function AlbumInfo({ album }) {
+  if (!album) {
+    return <div>Loading album details...</div>; // Handle null state
+  }
+
+  const artists =
+    album.artists?.map((artist) => artist.name).join(", ") || "Unknown Artist";
+
   return (
-    <div>
+    <div className="albumInfo-card">
       <div className="albumName-container">
-        <p></p>
+        <p>{`${album.name} - ${artists}`}</p>
       </div>
       <div className="album-info">
-        <p></p>
+        <p>
+          {album.label || "Unknown Label"} is an {album?.album_type} with{" "}
+          {album?.total_tracks} tracks
+        </p>{" "}
+        {/* Display Label Info */}
       </div>
       <div className="album-release">
-        <p></p>
+        <p>Release Date: {album.release_date || "Unknown Release Date"}</p>{" "}
+        {/* Display Release Date */}
       </div>
     </div>
   );
