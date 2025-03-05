979ddf53bf08bf5f9b531178b89b7531b2b6af62
Awontu Vannovich <awontuvannovich@gmail.com>
2025-03-05 07:45:42 +0100

    Making the Auth

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -9,21 +9,30 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Player from "../player/player";
 import Favorites from "../favorites/favorites";
 import Login from "../auth/Login";
+import { setClientToken } from "../../spotify";
 
 export default function Home() {
   const [token, setToken] = useState("");
 
   useEffect(() => {
+    const token = window.localStorage.getItem("token");
     const hash = window.location.hash;
-    const _token = hash.split("&")[0].split("=")[1];
-    window.localStorage.setItem("token", _token);
-    setToken(_token);
+    window.Location.hash = "";
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
   }, []);
   return !token ? (
     <Login />
   ) : (
     <BrowserRouter>
-      <div className="main-bod">
+      <div className="main-body">
         <Sidebar />
         <Routes>
           <Route path="/" element={<Library />} />
--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const authEndpoint = "https://accounts.spotify.com/authorize;";
 const clientID = "7c01e8c6b889469f8ea725dce16da227";
 const redirectUrl = "http://localhost:5173/";
@@ -8,3 +9,20 @@ const ClientSecret = "de204115908f4b6f9e05126d2dbe9f9a";
 export const LOGIN_URL = `${authEndpoint}?client_id=${clientID}&response_type=token&redirect_uri=${encodeURIComponent(
   redirectUrl
 )}&scope=${SCOPES}`;
+
+const apiClient = axios.create({
+  baseURL: "https://api.spotify.com/v1/",
+});
+
+// Function to set the Authorization token dynamically
+export const setClientToken = (token) => {
+  apiClient.interceptors.request.use(
+    async (config) => {
+      config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+};
+
+export default apiClient;
